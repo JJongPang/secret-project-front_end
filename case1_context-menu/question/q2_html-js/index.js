52aa95400417cd24c111a2d454f0bc88b7743d02
Jong-Pang <jjongpang92@gmail.com>
2021-03-07 21:24:34 +0900

    case1 q-html-js 내가 코딩한 답 제출

--- case1_context-menu/question/q2_html-js/index.js
+++ case1_context-menu/question/q2_html-js/index.js
@@ -1,1 +1,16 @@
-// Write Javascript code here!
+'use strict';
+
+const items = document.querySelectorAll('details');
+document.body.addEventListener('click', (e) => {
+    if (e.target.nodeName !== 'P' && e.target.nodeName !== 'SUMMARY') {
+        items.forEach((item) => {
+            item.removeAttribute('open');
+        });
+        return;
+    }
+    items.forEach((item) => {
+        if (item !== e.target.parentElement) {
+            item.removeAttribute('open');
+        }
+    });
+});
