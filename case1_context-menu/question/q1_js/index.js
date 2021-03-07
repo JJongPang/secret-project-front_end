5636a68f55f75c8372410609d84c976c8a2b842c
Jong-Pang <jjongpang92@gmail.com>
2021-03-07 20:25:59 +0900

    case1 q-js 내가 코딩한 답 제출

--- case1_context-menu/question/q1_js/index.js
+++ case1_context-menu/question/q1_js/index.js
@@ -1,1 +1,22 @@
 // Write Javascript code here!
+const wrapper = document.querySelector('.wrapper');
+const items = document.querySelectorAll('.item');
+
+wrapper.addEventListener('click', (e) => {
+    const target = e.target;
+    e.stopPropagation();
+    if (!target.classList.contains('item')) return;
+    target.classList.add('open');
+    items.forEach((elem) => {
+        if (elem !== target) {
+            elem.classList.remove('open');
+        }
+    });
+});
+
+document.body.addEventListener('click', (e) => {
+    if (e.target.classList.contains('context')) return;
+    items.forEach((item) => {
+        item.classList.remove('open');
+    });
+});
