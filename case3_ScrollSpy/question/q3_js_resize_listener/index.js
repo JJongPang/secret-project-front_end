2a6701119b67ec2abef45f9a3b1281648e171a84
Jong-Pang <jjongpang92@gmail.com>
2021-03-09 22:50:15 +0900

    case3_ScrollSpy q3.js_resize_listener 답 제출

--- case3_ScrollSpy/question/q3_js_resize_listener/index.js
+++ case3_ScrollSpy/question/q3_js_resize_listener/index.js
@@ -1,34 +1,38 @@
-const navElem = document.querySelector("#nav");
+const navElem = document.querySelector('#nav');
 const navItems = Array.from(navElem.children);
-const contentsElem = document.querySelector("#contents");
+const contentsElem = document.querySelector('#contents');
 const contentItems = Array.from(contentsElem.children);
 
 let offsetTops = [];
 const getOffsetTops = () => {
-  // do something
+    offsetTops = contentItems.map((elem) => {
+        const [ofs, clh] = [elem.offsetTop, elem.clientHeight];
+        return [ofs - clh / 2, ofs + clh / 2];
+    });
 };
+
 getOffsetTops();
 
-window.addEventListener("scroll", e => {
-  const { scrollTop } = e.target.scrollingElement;
-  const targetIndex = Math.max(
-    offsetTops.findIndex(([from, to]) => scrollTop >= from && scrollTop < to),
-    0
-  );
-  Array.from(navElem.children).forEach((c, i) => {
-    c.classList[i === targetIndex ? "add" : "remove"]("on");
-  });
+window.addEventListener('scroll', (e) => {
+    const { scrollTop } = e.target.scrollingElement;
+    const targetIndex = Math.max(
+        offsetTops.findIndex(([from, to]) => scrollTop >= from && scrollTop < to),
+        0
+    );
+    Array.from(navElem.children).forEach((c, i) => {
+        c.classList[i === targetIndex ? 'add' : 'remove']('on');
+    });
 });
 
-window.addEventListener("resize", getOffsetTops);
+window.addEventListener('resize', getOffsetTops);
 
-navElem.addEventListener("click", e => {
-  const targetElem = e.target;
-  if (targetElem.tagName === "BUTTON") {
-    const targetIndex = navItems.indexOf(targetElem.parentElement);
-    contentItems[targetIndex].scrollIntoView({
-      block: "start",
-      behavior: "smooth"
-    });
-  }
+navElem.addEventListener('click', (e) => {
+    const targetElem = e.target;
+    if (targetElem.tagName === 'BUTTON') {
+        const targetIndex = navItems.indexOf(targetElem.parentElement);
+        contentItems[targetIndex].scrollIntoView({
+            block: 'start',
+            behavior: 'smooth',
+        });
+    }
 });
