9812b55f7b8630d0dc4b3fec843f5c0c5bcba5b4
Jong-Pang <jjongpang92@gmail.com>
2021-03-09 23:13:27 +0900

    case3_ScrollSpy q4.js_throttle 답 제출

--- case3_ScrollSpy/question/q4_js_throttle/util.js
+++ case3_ScrollSpy/question/q4_js_throttle/util.js
@@ -1,12 +1,21 @@
 export const debounce = (func, delay) => {
-  let timeoutId = null;
-  return (...arg) => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(func.bind(null, ...arg), delay);
-  };
+    let timeoutId = null;
+    return (...arg) => {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(func.bind(null, ...arg), delay);
+    };
 };
 
 export const throttle = (func, ms) => {
-  let throttled = false;
-  // do something
+    let throttled = false;
+    // do something
+    return (...args) => {
+        if (!throttled) {
+            throttled = true;
+            setTimeout(() => {
+                func(...args);
+                throttled = false;
+            }, delay);
+        }
+    };
 };
