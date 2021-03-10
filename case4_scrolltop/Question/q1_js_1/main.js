a2fd8515b64a7858a6a21ec51d73026960c60fa4
Jong-Pang <jjongpang92@gmail.com>
2021-03-10 21:15:56 +0900

    case4_scrolltop q2.js 답 제출

--- case4_scrolltop/Question/q1_js_1/main.js
+++ case4_scrolltop/Question/q1_js_1/main.js
@@ -1,6 +1,5 @@
 const nav = document.querySelector('nav');
 
-const navHeight = nav.getBoundingClientRect().height;
 // 첫 번째 방법
 // 스크롤 이벤트가 발생했을 때
 
@@ -9,6 +8,8 @@ const navHeight = nav.getBoundingClientRect().height;
 // 스크롤 위치를 바탕으로 active 클래스를 추가하거나 제거한다.
 
 // Write JS Code Here!
+//내가 푼 답
+// const navHeight = nav.getBoundingClientRect().height;
 // document.addEventListener('scroll', (e) => {
 //     const { scrollTop } = e.target.scrollingElement;
 //     if (scrollTop > navHeight) {
@@ -18,16 +19,22 @@ const navHeight = nav.getBoundingClientRect().height;
 //     }
 // });
 
-let position = 0;
-document.addEventListener('scroll', (e) => {
-    const { scrollTop } = e.target.scrollingElement;
-    if (scrollTop > navHeight) {
-        nav.classList.add('active');
-        position = scrollTop;
-    }
-    if (scrollTop < position) {
-        nav.classList.remove('active');
-        console.log('scrollTop', scrollTop);
-        console.log(position);
-    }
+// 내가 푼 답
+// let position = 0;
+// document.addEventListener('scroll', (e) => {
+//     const { scrollTop } = e.target.scrollingElement;
+//     if (scrollTop > navHeight) {
+//         nav.classList.add('active');
+//         position = scrollTop;
+//     }
+//     if (scrollTop < position) {
+//         nav.classList.remove('active');
+//         console.log('scrollTop', scrollTop);
+//         console.log(position);
+//     }
+// });
+
+window.addEventListener('scroll', (e) => {
+    const top = window.scrollY || window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+    top >= 50 ? nav.classList.add('active') : nav.classList.remove('active');
 });
--- case4_scrolltop/Question/q2_js_2/main.js
+++ case4_scrolltop/Question/q2_js_2/main.js
@@ -5,10 +5,20 @@ let oldValue = 0;
 
 // 스크롤 이벤트가 발생했을 때
 
-    // 현재 스크롤 위치를 가져온다.
+// 현재 스크롤 위치를 가져온다.
 
-    // oldValue, 스크롤의 위치와 연산 작업을 하여 active 클래스를 추가하거나 제거한다.
+// oldValue, 스크롤의 위치와 연산 작업을 하여 active 클래스를 추가하거나 제거한다.
 
-    // oldValue를 스크롤 위치로 변경한다.
+// oldValue를 스크롤 위치로 변경한다.
 
 //Write JS Code Here!
+window.addEventListener('scroll', (e) => {
+    const newValue = window.scrollY || window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+    if (oldValue - newValue < 0) {
+        nav.classList.add('active');
+    }
+    if (oldValue - newValue > 0) {
+        nav.classList.remove('active');
+    }
+    oldValue = newValue;
+});
