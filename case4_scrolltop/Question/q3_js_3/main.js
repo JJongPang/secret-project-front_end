6cd40cce696712e1b8173f02f56236b69d91c913
Jong-Pang <jjongpang92@gmail.com>
2021-03-10 21:30:01 +0900

    case4_scrolltop q3.js 답 제출

--- case4_scrolltop/Question/q3_js_3/main.js
+++ case4_scrolltop/Question/q3_js_3/main.js
@@ -1,4 +1,4 @@
-const isFireFox = (navigator.userAgent.indexOf('Firefox') !== -1);
+const isFireFox = navigator.userAgent.indexOf('Firefox') !== -1;
 const wheelEvt = isFireFox ? 'DOMMouseScroll' : 'wheel';
 window.addEventListener(wheelEvt, mouseWheelEvent);
 
@@ -6,10 +6,12 @@ window.addEventListener(wheelEvt, mouseWheelEvent);
 
 // 스크롤 이벤트가 발생했을 때
 
-    // 현재 스크롤 위치를 가져온다.
-    // 스크롤 위치를 바탕으로 active 클래스를 추가하거나 제거한다.
+// 현재 스크롤 위치를 가져온다.
+// 스크롤 위치를 바탕으로 active 클래스를 추가하거나 제거한다.
 
+const nav = document.querySelector('nav');
 function mouseWheelEvent(e) {
     // Write JS Code Here!
-
+    const delta = e.wheelDelta ? e.wheelDelta : e.delta;
+    delta < 0 ? nav.classList.add('active') : nav.classList.remove('active');
 }
