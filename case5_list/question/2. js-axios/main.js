aefbf627c3fc58f612e95306924e86576a76eb5d
Jong-Pang <jjongpang92@gmail.com>
2021-03-10 22:28:34 +0900

    case4_List q1-js-axios제출

--- case5_list/question/2. js-axios/main.js
+++ case5_list/question/2. js-axios/main.js
@@ -1,33 +1,43 @@
 const ul = document.querySelector('ul');
 const API = 'https://jsonplaceholder.typicode.com/posts';
 
-
 // Axios : https://github.com/axios/axios
 // q2-1. Javascript - axios 라이브러리를 사용하여 해당 기능을 구현하세요.
 // Do something here!
 
+// axios
+//     .get(API) //
+//     .then((res) => {
+//         const { data } = res;
+//         data.forEach((item) => {
+//             const li = `<li>
+//                             <h2>${item.title}</h2>
+//                             <p>${item.body}</p>
+//                         </li>`;
+//             ul.insertAdjacentHTML('beforeend', li);
+//         });
+//     });
 
-
-
-
-// q2-2. Axios Refactoring
-// 템플릿
-const printPosts = res => {
-    const {data} = res;
-    data.forEach(item => {
+//q2-2. Axios Refactoring
+//템플릿
+const printPosts = (res) => {
+    const { data } = res;
+    data.forEach((item) => {
         const li = `<li>
                         <h2>${item.title}</h2>
                         <p>${item.body}</p>
                     </li>`;
-        ul.insertAdjacentHTML("beforeend", li);
-    })
-}
+        ul.insertAdjacentHTML('beforeend', li);
+    });
+};
 
 // HTTP 통신
 const fetchPosts = (url) => {
     // Do something here!
+    return axios.get(url);
+};
 
-}
-
-fetchPosts(API)
-    // Do something here!
+fetchPosts(API) //
+    .then(printPosts)
+    .catch((e) => console.log(e));
+//Do something here!
