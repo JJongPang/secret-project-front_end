ed8cd6156303c3333f52054c9fff13d8a777fadb
Jong-Pang <jjongpang92@gmail.com>
2021-03-15 01:54:10 +0900

    case12_Carousel init, q1-js, q3-react 답 제출

---
+++ case12_carousel-1/qusetion/q3_react/src/Carousel.js
@@ -0,0 +1,87 @@
+import { useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
+import Item from './Item';
+import Button from './Button';
+
+const CarouselStyle = styled.div`
+    overflow: hidden;
+    width: 90%;
+    margin: auto;
+    * {
+        box-sizing: border-box;
+    }
+    .carousel {
+        -webkit-transform-style: preserve-3d;
+        -moz-transform-style: preserve-3d;
+        transform-style: preserve-3d;
+    }
+`;
+
+export default function Carousel() {
+    const totalItems = 5;
+    const [current, setCurrent] = useState(0);
+
+    const isMoving = useRef(false);
+    useEffect(() => {
+        isMoving.current = true;
+        setTimeout(() => {
+            isMoving.current = false;
+        }, 500);
+    }, [current]);
+
+    // 다음 슬라이드로 이동
+    const moveNext = () => {
+        // Do Something Here!
+        if (!isMoving.current) {
+            if (current === totalItems - 1) {
+                setCurrent(0);
+            } else {
+                setCurrent(current + 1);
+            }
+        }
+    };
+
+    // 이전슬라이드로 이동
+    const movePrev = () => {
+        // Do Something Here!
+        if (!isMoving.current) {
+            if (current === 0) {
+                setCurrent(totalItems - 1);
+            } else {
+                setCurrent(current - 1);
+            }
+        }
+    };
+
+    const ItemList = Array(totalItems)
+        .fill()
+        .map((_, index) => {
+            const key = `item_${index}`;
+
+            // prev 와 next 상태를 작성
+            // Do Something Here!
+            const prev = current === 0 ? totalItems - 1 : current - 1;
+            const next = current === totalItems - 1 ? 0 : current + 1;
+
+            return (
+                <Item
+                    src={`https://picsum.photos/id/${index}/1600/900`}
+                    key={key}
+                    active={index === current}
+                    prev={index === prev}
+                    next={index === next}
+                />
+            );
+        });
+
+    return (
+        <CarouselStyle>
+            <div className="carousel">
+                {ItemList}
+
+                <Button prev handleSlide={movePrev} />
+                <Button next handleSlide={moveNext} />
+            </div>
+        </CarouselStyle>
+    );
+}
