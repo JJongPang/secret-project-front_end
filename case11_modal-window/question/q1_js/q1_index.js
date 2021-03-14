1e3cc6ed5373be5513d9816b585844a028afd4fe
Jong-Pang <jjongpang92@gmail.com>
2021-03-14 23:41:16 +0900

    case10_Modal q1-js 답 제출

--- case11_modal-window/question/q1_js/q1_index.js
+++ case11_modal-window/question/q1_js/q1_index.js
@@ -6,15 +6,18 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.popup-trigger').addEventListener('click', function () {
         // 팝업 열기를 클릭했을 때 모달을 띄우도록 구현하세요.onSetIsVisible
         popupModal.classList.add('is--visible');
+        bodyBlackout.classList.add('is-blacked-out');
     });
 
     popupModal.querySelector('.popup-modal__close').addEventListener('click', () => {
         // 닫기 버튼을 눌렀을 때 작동하도록 구현하세요.
         popupModal.classList.remove('is--visible');
+        bodyBlackout.classList.remove('is-blacked-out');
     });
 
     bodyBlackout.addEventListener('click', () => {
         // 모달 바탕을 클릭 했을 때 작동하도록 구현하세요.
         popupModal.classList.remove('is--visible');
+        bodyBlackout.classList.remove('is-blacked-out');
     });
 });
